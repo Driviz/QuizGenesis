1110999b92311df1a70b958105f2a9233a0a116b
Drgit config --global user.email youremail@yourdomain.comiviz <kumar.driviz@gmail.com>
2020-08-18 18:14:04 +0530

    added Results Model and added permissions to Quiz apis

--- QuizApi/api/admin.py
+++ QuizApi/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import QuizCategories, Quiz, Options, Questions, QuizSubCategories
+from .models import QuizCategories, Quiz, Options, Questions, QuizSubCategories, Results
 
 # Register your models here.
 admin.site.register(QuizCategories)
@@ -7,4 +7,5 @@ admin.site.register(QuizSubCategories)
 admin.site.register(Quiz)
 admin.site.register(Questions)
 admin.site.register(Options)
+admin.site.register(Results)
 
